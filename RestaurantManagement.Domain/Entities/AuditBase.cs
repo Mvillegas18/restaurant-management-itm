f02d8e15c14054178fa38e5f274529f9b84dff0a
Valeria-Hibla <valeriarodriguez1127989@correo.itm.edu.co>
2026-05-16 14:16:39 -0500

    Entity AuditBase was created

---
+++ RestaurantManagement.Domain/Entities/AuditBase.cs
@@ -0,0 +1,7 @@
+﻿namespace RestaurantManagement.Domain.Entities;
+public abstract class AuditBase
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; }
+}
