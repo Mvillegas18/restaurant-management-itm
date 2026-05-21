1afc840741076eff4bf5630e5c09574440b4e2fb
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-05-21 15:07:27 -0500

    Configure DI, EF Core, Swagger, and DB seeding
    Set up dependency injection for repositories and services, configure EF Core with SQL Server, enable Swagger for API docs, apply migrations and seed data on startup, redirect root to Swagger UI, and add connection string to appsettings.json.

--- RestaurantManagement.API/Program.cs
+++ RestaurantManagement.API/Program.cs
@@ -1,18 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using RestauranteAPI.Domain.Services;
+using RestaurantManagement.DataAccess.Context;
+using RestaurantManagement.DataAccess.Repositories;
+using RestaurantManagement.DataAccess.Seeders;
+using RestaurantManagement.Domain.Interfaces;
+using RestaurantManagement.Domain.Interfaces.Services;
+using RestaurantManagement.Domain.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<RestaurantManagementDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IMenuItemRepository, MenuItemRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IReservationRepository, ReservationRepository>();
+builder.Services.AddScoped<ITableRepository, TableRepository>();
+
+builder.Services.AddScoped<IMenuItemService, MenuItemService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IReservationService, ReservationService>();
+builder.Services.AddScoped<ITableService, TableService>();
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider
+        .GetRequiredService<RestaurantManagementDbContext>();
+
+    await context.Database.MigrateAsync();
+    await DataSeeder.SeedAsync(context);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
 app.UseHttpsRedirection();
 
