3da4b4a513e3ba2aa465c49b3f3c5fff85cd046b
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2026-06-01 17:22:19 -0500

    add: new frontend files

--- restaurant-management-frontend/src/app/app.routes.ts
+++ restaurant-management-frontend/src/app/app.routes.ts
@@ -1,20 +1,12 @@
 import { Routes } from '@angular/router';
-import { AuthPageComponent } from './features/auth/auth.page';
 import { CustomersPageComponent } from './features/customers/customers.page';
 import { MenuItemsPageComponent } from './features/menu-items/menu-items.page';
-import { OrdersPageComponent } from './features/orders/orders.page';
-import { ReservationsPageComponent } from './features/reservations/reservations.page';
 import { RestaurantsPageComponent } from './features/restaurants/restaurants.page';
-import { TablesPageComponent } from './features/tables/tables.page';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'restaurants' },
-  { path: 'auth', component: AuthPageComponent },
   { path: 'restaurants', component: RestaurantsPageComponent },
   { path: 'menu-items', component: MenuItemsPageComponent },
-  { path: 'tables', component: TablesPageComponent },
-  { path: 'reservations', component: ReservationsPageComponent },
-  { path: 'orders', component: OrdersPageComponent },
   { path: 'customers', component: CustomersPageComponent },
   { path: '**', redirectTo: 'restaurants' }
 ];
